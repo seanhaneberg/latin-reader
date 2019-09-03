2671e11e55a73c95ea35d3ae03f684527507e41c
Sean Haneberg <haneberg@gmail.com>
2019-09-02 21:39:05 -0700

    naive viz

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import './App.css';
+import Catullus from './translations/catullus-13.json'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          Latin Reader
-      </header>
-    </div>
+    return (
+        <div className="App">
+            <header className="App-header">
+                {Catullus.name}
+            </header>
+            <div>
+                <div>
+                    Author: {Catullus.author}
+                </div>
+                <div>
+                    Original: {Catullus.original}
+                </div>
+                <div>
+                    Translation: {Catullus.literal_translation}
+                </div>
+            </div>
+        </div>
   );
 }
 
